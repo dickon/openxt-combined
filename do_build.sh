cd941ad2d8e3213a8181660d0b15ff135403d829
Dickon Reed <dickon@cantab.net>
2014-06-13 15:18:19 +0100

    move repos to build/oe/repos where xenclient-oe/conf/bblayers.conf assumes it is

--- do_build.sh
+++ do_build.sh
@@ -197,9 +197,9 @@ EOF
 	    OPTS=""
 	fi
 
-	pushd ${TOPDIR}/build
+	pushd ${TOPDIR}/build/oe
 	# TODO: inline setup_build here?
-	../xenclient-oe/setup_build $OPTS
+	../../xenclient-oe/setup_build $OPTS
 	mkdir -p oe/conf
 	if [ ! -e oe/conf/bblayers.conf ]; then
 	    ln -s ../../../xenclient-oe/conf/bblayers.conf oe/conf/bblayers.conf
